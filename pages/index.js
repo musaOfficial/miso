f6681c8c1b97b0780e9e76d1900fd5f1972aa888
Musa Arslan <officialmusa58@gmail.com>
2024-04-07 19:00:33 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { UserButton } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
+
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       <UserButton afterSignOutUrl="/" />
+      This is the user
+      
     </main>
   );
 }
