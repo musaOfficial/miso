20c54fb17e03e6976c9138a3f8052e19c131c798
Musa Arslan <officialmusa58@gmail.com>
2024-04-07 18:55:55 +0200

    Message

--- middleware.js
+++ middleware.js
@@ -1,8 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ["/"],
-  ignoredRoutes: ["/api/webhooks"]
+  publicRoutes: ["/api/webhooks", "/"]
 });
  
 export const config = {
